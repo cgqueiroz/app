fe62083e8ec2547e495453a43f4e054eb20f7328
camila <camilagqueiroz@outlook.com>
2024-08-21 22:27:01 -0300

    atualizações

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,28 +1,45 @@
 import { Tabs } from 'expo-router';
+//importa o componente tabs do modulo 'expo-router', que é usado para criar uma navegação de abas na aplicação
 
 import { TabBarIcon } from '~/components/TabBarIcon';
+// importa o componente 'tabbaricon' de um caminho relativo, este componente será usado pora renderizar icones personalizados na barra de abas
+// opção de colocar a foto de perfil do usuario e notificações (bolinha vermelha)
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: 'black',
-      }}>
+        headerShown: false, // esconde o cabeçalho padrão;
+        tabBarActiveTintColor: 'blue', // define a cor de texto dos icones;
+      }}> 
+      {/* 
+        o componente tabs organiza a nagevação da app em um layout de abas 
+        as ações screenoptions configuram o comportamento e a aparência das abas
+      */}
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
+      {/* 
+      define a primeira aba com o nome da rota index
+      title: tab one define o titulo da aba
+      tabbarion utiliza o componente tabbarion para renderizar um icone com base na cor escolhida
+      */}
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Heart',
+          tabBarIcon: ({ color }) => <TabBarIcon name="heart" color={color} />,
         }}
       />
+            {/* 
+      define a segunda aba com o nome da rota two
+      title: tab two define o titulo da aba
+      tabbarion utiliza o componente tabbarion para renderizar um icone com base na cor escolhida
+      */}
     </Tabs>
   );
 }
--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,7 +1,11 @@
 import { Stack } from 'expo-router';
+// importa o componente stack do modulo expo-router, usado para configurar a navegação em pilha (stack navigation)
 
 import { Container } from '~/components/Container';
+// importa o componente container de umn caminho relativo, geralmente usado para organizar e centralizar o conteudo da tela 
+
 import { ScreenContent } from '~/components/ScreenContent';
+// importa o componente screencontent que provavelmente é responsável por exibir o conteúdo especifico da tela, como texto e elementos visuais. 
 
 export default function Home() {
   return (
